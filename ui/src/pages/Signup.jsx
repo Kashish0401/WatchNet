b2f73d4b3515f48bff760fcc7e6ec1fd9f636fc9
Kashish <kashish042001@gmail.com>
2023-04-04 03:36:58 +0530

    ui designing

--- ui/src/pages/Signup.jsx
+++ ui/src/pages/Signup.jsx
@@ -7,19 +7,19 @@ function Signup() {
     <div>
       <Header/>
     <div className="content">
-    <div className='details'>
-      <h1>Welcom to WatchNet</h1>
-      <h3>Watch the latest movies online anywhere, anytime for free</h3>
-      <h6>Ready to watch?</h6>
-      <div className="form">
-        <input type="email" placeholder='Email Address' name='email'/>
-        <input type="password" placeholder='password' name='password' />
-        <button>Get Started</button>
+      <div className='details'>
+        <h1>Welcom to WatchNet</h1>
+        <h3>Watch the latest movies online anywhere, anytime for free</h3>
+        <h6>Ready to watch?</h6>
+        <div className="form">
+          <input type="email" placeholder='Email Address' name='email'/>
+          <input type="password" placeholder='password' name='password' />
+          <button>Get Started</button>
+        </div>
+        <div className="image">
+          <img src={image} alt="" />
+        </div>
       </div>
-      <div className="image">
-        <img src={image} alt="" />
-      </div>
-    </div>
     </div>
     </div>
   )
