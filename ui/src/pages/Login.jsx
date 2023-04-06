4118e3e062d57be69544254ddf0f1d23842f9925
Kashish <kashish042001@gmail.com>
2023-04-06 19:12:29 +0530

    login

--- ui/src/pages/Login.jsx
+++ ui/src/pages/Login.jsx
@@ -1,11 +1,66 @@
 import Header from "../components/Header";
+import '../Styles/SignupStyles.css'
+import image from "../Assets/signup.jpg";
+import { useState } from "react";
+import { firebaseAuth } from "../utils/firebase-config";
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [formValues, setFormValues] = useState({email: "", password: ""});
+
+  const handleLogin = async () => {
+    try{
+      const {email, password} = formValues;
+      await signInWithEmailAndPassword(firebaseAuth,email,password)
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  onAuthStateChanged(firebaseAuth,(currentUser) => {
+    if(currentUser) 
+      navigate("/")
+  });
   return (
-    <div>
+    <>
       <Header/>
+      <div className="mask">
+      <img src={image} alt="" className="image"/>
+      <div className="content">
+        <div className='details'>
+          <h1>Welcome to WatchNet</h1>
+          <h3>Watch the latest movies online anywhere, anytime for free</h3>
+          <h6>Ready to watch?</h6>
+          <div className="form">
+            <input type="email" 
+            placeholder='Email Address' 
+            name='email' 
+            value={formValues.email} 
+            onChange={(e) => setFormValues({...formValues, [e.target.name]: e.target.value,})
+            }
+            />
+            <input type="password" 
+            placeholder='password' 
+            name='password' 
+            value={formValues.password} 
+            onChange={(e) => setFormValues({...formValues, [e.target.name]: e.target.value,})
+            }
+            />
+            <button onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
 
-export default Login;
+export default Login
+
--- ui/src/pages/Signup.jsx
+++ ui/src/pages/Signup.jsx
@@ -1,28 +1,64 @@
 import Header from "../components/Header";
 import '../Styles/SignupStyles.css'
 import image from "../Assets/signup.jpg";
+import { useState } from "react";
+import { firebaseAuth } from "../utils/firebase-config";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-const signup = () => {
+const Signup = () => {
+  const navigate = useNavigate();
+  const [formValues, setFormValues] = useState({email: "", password: ""});
+
+  const handleSignin = async () => {
+    try{
+      const {email, password} = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth,email,password)
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  onAuthStateChanged(firebaseAuth,(currentUser) => {
+    if(currentUser) 
+      navigate("/")
+  });
+
   return (
     <>
-        <Header/>
-        <div className="mask">
-      <img src={image} alt="" className="image"/>
-    <div className="content">
-      <div className='details'>
-        <h1>Welcome to WatchNet</h1>
-        <h3>Watch the latest movies online anywhere, anytime for free</h3>
-        <h6>Ready to watch?</h6>
-        <div className="form">
-          <input type="email" placeholder='Email Address' name='email'/>
-          <input type="password" placeholder='password' name='password' />
-          <button>Get Started</button>
-        </div>
+      <Header/>
+      <div className="mask">
+        <img src={image} alt="" className="image"/>
+        <div className="content">
+          <div className='details'>
+            <h1>Welcome to WatchNet</h1>
+            <h3>Watch the latest movies online anywhere, anytime for free</h3>
+            <h6>Ready to watch?</h6>
+            <div className="form">
+              <input type="email" 
+              placeholder='Email Address' 
+              name='email' value={formValues.email} 
+              onChange={
+                (e) => setFormValues({...formValues, [e.target.name]: e.target.value,})
+              }/>
+              <input type="password" 
+              placeholder='password' 
+              name='password' 
+              value={formValues.password} 
+              onChange={
+                (e) => setFormValues({...formValues, [e.target.name]: e.target.value,})
+                }/>
+              <button onClick={handleSignin}>Sign Up</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
 
-export default signup
+export default Signup
