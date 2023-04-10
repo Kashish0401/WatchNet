cf40b3269399a415ec9e1b35ea0313a5b76ed6b9
Kashish <kashish042001@gmail.com>
2023-04-10 08:41:40 +0530

    Homepage

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
+import seriesPoster from "../Assets/SeriesPoster.jpg";
+import SeriesTitle from "../Assets/SeriesTitle.png"
+import { FaPlay } from 'react-icons/fa';
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import '../Styles/Home.css'
 
 function Home() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
-    <div>
-      <Header/>
-    </div>
+    <>
+      <Header isScrolled={isScrolled}/>
+      <div className='hero'>
+        <img 
+        src={seriesPoster} 
+        alt="Poster" 
+        className='poster'
+        />
+        <div className='info'>
+          <div className='seriesTitle'>
+            <img src={SeriesTitle} alt="Title" className='title'/>
+          </div>
+          <div className='heroButtons'>
+            <button>
+              <FaPlay/>
+              Play
+            </button>
+            <button>
+              <AiOutlineInfoCircle/>
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
