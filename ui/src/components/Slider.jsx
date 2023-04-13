0c82acdb1d20580f25151fea9b887a7e32314156
Kashish <kashish042001@gmail.com>
2023-04-13 19:00:19 +0530

    Slider component

---
+++ ui/src/components/Slider.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import CardSlider from './CardSlider';
+const Slider = () => {
+
+  const getMoviesFromRange = (from, to) => {
+    return movies.slice(from, to);
+  };
+
+  return (
+    <>
+      <CardSlider data={getMoviesFromRange(0, 10)} title="Trending Now" />
+      <CardSlider data={getMoviesFromRange(10, 20)} title="New Releases" />
+      <CardSlider
+        data={getMoviesFromRange(20, 30)}
+        title="Blockbuster Movies"
+      />
+      <CardSlider
+        data={getMoviesFromRange(30, 40)}
+        title="Popular"
+      />
+      <CardSlider data={getMoviesFromRange(40, 50)} title="Action Movies" />
+      <CardSlider data={getMoviesFromRange(50, 60)} title="Epics" />
+    </>
+  )
+}
+
+export default Slider;
