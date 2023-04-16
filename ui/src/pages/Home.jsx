4d361dff3443cf8ec91e3811a6993fd5d2225758
Kashish <kashish042001@gmail.com>
2023-04-16 16:54:07 +0530

    api fetching

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -6,27 +6,52 @@ import { FaPlay } from 'react-icons/fa';
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import '../Styles/Home.css'
 import { useNavigate } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchMovies, getGenres } from '../store';
+import { API_KEY, TMBD_BASE_URL } from "../utils/Constants";
+import axios from 'axios';
 import Slider from '../components/Slider';
 
 function Home() {
   const [isScrolled, setIsScrolled] = useState(false);
   const navigate = useNavigate();
-  /*const genresLoaded = useSelector((state) => state.watchNet.genresLoaded);
-  //const movies = useSelector((state) => state.WatchNet.movies);
-  //const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getGenres());
-  }, []);
-    useEffect(() => {
-      if (genresLoaded) dispatch(fetchMovies({ type: "all" }));
-    }, [genresLoaded]);
-  */
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+    fetchMoviesGenre();
+  }, []);
+
+  const [movie] = useState([]);
+
+  const fetchMovies = async () => {
+    const data = await axios.get(
+      `${TMBD_BASE_URL}/trending/all/week?api_key=${API_KEY}`
+    );
+    const { results } = data.data;
+    results.map((e) => {
+      const { original_title, backdrop_path } = e;
+      if (original_title) {
+        movie.push({ original_title, backdrop_path });
+      }
+    });
+    //console.log(trendingMovie);
+  }
+
+  const fetchMoviesGenre = async () => {
+    const data = await axios.get(
+      `${TMBD_BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=10749`
+    );
+    //console.log(data);
+    const { results } = data.data;
+    results.map((e) => {
+      const { original_title, backdrop_path } = e;
+      if (original_title) {
+        movie.push({ original_title, backdrop_path });
+      }
+    });
   };
 
   return (
@@ -55,7 +80,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <Slider />
+      <Slider title='Trending Now' data={movie} />
     </>
   );
 }
