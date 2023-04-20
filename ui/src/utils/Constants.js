bd0c8c0d9c340c353a111f6ffc8bef7a1b9b86d1
Kashish <kashish042001@gmail.com>
2023-04-21 00:45:07 +0530

    cleanup

--- ui/src/utils/Constants.js
+++ ui/src/utils/Constants.js
@@ -1,2 +1,2 @@
 export const API_KEY = "a7a5cc051df16a83aeb32726a57da92c";
-export const TMBD_BASE_URL = "https://api.themoviedb.org/3";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
