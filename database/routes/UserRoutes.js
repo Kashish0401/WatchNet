81e6cbc058b48eaad8ff1c4edbf66abcaa71d870
Kashish <kashish042001@gmail.com>
2023-04-23 02:01:06 +0530

    add to list

--- database/routes/UserRoutes.js
+++ database/routes/UserRoutes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 
-const { addToLikedMovies } = require("../controllers/UserController");
+const { addToLikedMovies, getLikedMovies } = require("../controllers/UserController");
 
 router.post("/add", addToLikedMovies)
+router.get("/liked/:email",getLikedMovies)
 
 module.exports = router;
--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import seriesPoster from "../Assets/SeriesPoster.jpg";
 import SeriesTitle from "../Assets/SeriesTitle.png"
