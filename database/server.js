fa53d1e38ea6d4167e3f4d145a8c6180407160aa
Kashish <kashish042001@gmail.com>
2023-04-22 12:34:56 +0530

    add to list

--- database/server.js
+++ database/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose") 
-
+const userRoutes = require("./routes/UserRoutes");
 
 const app = express();
 
@@ -11,5 +11,7 @@ app.use(express.json());
 mongoose.connect("mongodb://0.0.0.0/watchnet", { useNewUrlParser: true, useUnifiedTopology: true, }).then(() => {
     console.log("DB connected");
 });
+
+app.use("/database/user",userRoutes)
 
 app.listen(5000, console.log("Worked"));
